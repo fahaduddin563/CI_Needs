cdbe480e425453ff52c0423842feb122ab4fc737
Sky Hampton <forrest.hampton744@myci.csuci.edu>
2026-03-04 15:30:39 -0800

    Added field to post table to determine post type (offer or request)

--- CINeeds_DDL.sql
+++ CINeeds_DDL.sql
@@ -2,19 +2,22 @@ DROP DATABASE IF EXISTS CINeeds;
 CREATE DATABASE CINeeds;
 USE CINeeds;
 
+--Table containing user login info
 CREATE TABLE CIN_User (
 userID INT,
 username VARCHAR(32),
 password VARCHAR(32),
 PRIMARY KEY (userID));
 
+--Table containing all data required for posts
 CREATE TABLE CIN_Post (
 postID INT,
 userID INT,
-category VARCHAR(16),
+postType VARCHAR(8), --either REQUEST or OFFER
+category VARCHAR(16), --can be the following values: food, housing, financial, health, academic, other
 postTitle VARCHAR(32),
 postData TINYTEXT,
 postDate DATE,
-offerExpDate DATE,
+offerExpDate DATE, --can be null if not an offer
 PRIMARY KEY (postID),
 FOREIGN KEY (userId) REFERENCES CIN_User(userID));
